8cb705148d2efddd2dc75cea1106a721d39bace6
aniss <aniss@qq.com>
2024-10-15 13:24:19 +0800

    修复 测试用例报错

--- core/agents/spotify/client_test.go
+++ core/agents/spotify/client_test.go
@@ -93,7 +93,7 @@ var _ = Describe("client", func() {
 			})
 
 			_, err := client.authorize(context.TODO())
-			Expect(err).To(BeNil())
+			Expect(err).To(MatchError("spotify error(invalid_client): Invalid client"))
 		})
 
 		It("fails on invalid JSON response", func() {
